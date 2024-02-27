9b264dfae1b03b613fec2805b6427c5484295621
Ntkfxy <153200314+Ntkfxy@users.noreply.github.com>
2024-02-28 01:48:25 +0700

    UpdateQueue.php

--- UpdateQueue.php
+++ UpdateQueue.php
@@ -1,45 +1,49 @@
 <?php
 
+// Always include the connection file
+require 'conn.php';
 
-if (isset($_POST['QNumber']) && ($_POST['Qdate']))
-  require 'conn.php';
+// Check if QNumber is set in the POST request
+if (isset($_POST['QNumber'])) {
+  $sql =  "UPDATE queue 
+            SET QStatus = :QStatus,
+            Qdate = :Qdate
+            WHERE QNumber = :QNumber";
 
+  $stmt = $conn->prepare($sql);
+  $stmt->bindParam(':QStatus', $_POST['QStatus']);
+  $stmt->bindParam(':QNumber', $_POST['QNumber']);
+  $stmt->bindParam(':Qdate', $_POST['Qdate']);
 
-
-$sql =  "UPDATE queue 
-        SET QStatus = :QStatus,
-        pid =:pid
-          WHERE QNumber =:QNumber and  Qdate =:Qdate";
-$stmt = $conn->prepare($sql);
-$stmt->bindParam(':QStatus', $_POST['QStatus']);
-$stmt->bindParam(':pid', $_POST['pid']);
-$stmt->bindParam(':QNumber', $_POST['QNumber']);
-$stmt->bindParam(':Qdate', $_POST['Qdate']);
-
-
-echo '
-    <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert-dev.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css">';
-
-if ($stmt->execute()) {
   echo '
-      <script type="text/javascript">
+        <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert-dev.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css">';
 
-        $(document).ready(function(){
-        
-            swal({
-                title: "Success!",
-                text: "Successfuly update customer information",
-                type: "success",
-                timer: 2500,
-                showConfirmButton: false
-              }, function(){
-                    window.location.href = "index.php";
-              });
-        });
-        
-        </script>
-        ';
+  try {
+    if ($stmt->execute()) {
+      echo '
+                <script type="text/javascript">
+
+                    $(document).ready(function(){
+                    
+                        swal({
+                            title: "Success!",
+                            text: "Successfully update customer information",
+                            type: "success",
+                            timer: 2500,
+                            showConfirmButton: false
+                        }, function(){
+                            window.location.href = "index.php";
+                        });
+                    });
+                    
+                </script>
+                ';
+    } else {
+      echo 'Failed to execute the update statement.';
+    }
+  } catch (PDOException $e) {
+    echo 'Error: ' . $e->getMessage();
+  }
 }
-$conn = null;
